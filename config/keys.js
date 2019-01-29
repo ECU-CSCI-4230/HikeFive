4ade992947895c66621fcdc8f4876728550ab4a0
Madison <mtoquinn@gmail.com>
2019-01-29 16:55:50 -0500

    added the key

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 // Replace with actual keys to start the application.
 module.exports = {
-  mongoURI: '',
-  secretOrKey: ''
+  mongoURI: 'mongodb://isaiah6208:isaiahis13@ds135993.mlab.com:35993/randomongodb1',
+  secretOrKey: 'secret'
 };
