4cd4b2f3a5628eda77b26cd7d94e24fa65cb3261
Logan Dail <logan.dail6@yahoo.com>
2019-02-12 17:14:09 -0500

    Center Settings Page

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -38,14 +38,21 @@ class Dashboard extends Component {
               <Link to={`/personfeed/${profile.handle}`} >Person Wall</Link>
             </p>
 
-            <ProfileActions />
-            <div style={{  marginBottom: '60px' }} />
-            <button
-              onClick={ this.onDeleteClick.bind(this)}
-              className="btn btn-danger"
-            >
-              Delete My Account
-            </button>
+            <div className="btn-group-vertical d-flex flex-wrap justify-content-center align-items-center " role="group">
+              <Link to="/edit-profile" className="btn btn-light">
+               <i className="fas fa-user-circle text-info mr-1" /> Edit Profile
+              </Link>
+              <br></br>
+              <Link to="/Notification-Settings" className="btn btn-light">
+                <i className="fas fa-exclamation text-info mr-1" /> Notification Settings
+              </Link>
+              <br></br>
+              <Link to="change-password" className="btn btn-light">
+                <i className="fas fa-key text-info mr-1" /> Change Password
+              </Link>
+              <br></br>
+              <button onClick={ this.onDeleteClick.bind(this)} className="btn btn-danger">Delete My Account </button>
+            </div>          
           </div>
         );
       } else {
@@ -67,7 +74,7 @@ class Dashboard extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-12">
-              <h1 className="display-4">Account Settings</h1>
+              <h1 className="d-flex flex-wrap justify-content-center align-items-center display-4">Settings</h1>
               {dashboardContent}
             </div>
           </div>
--- client/src/components/dashboard/ProfileActions.js
+++ client/src/components/dashboard/ProfileActions.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const ProfileActions = () => {
   return (
-    <div className="btn-group-vertical" role="group">
+    <div className="btn-group-vertical d-flex flex-wrap justify-content-center align-items-center " role="group">
       <Link to="/edit-profile" className="btn btn-light">
         <i className="fas fa-user-circle text-info mr-1" /> Edit Profile
       </Link>
