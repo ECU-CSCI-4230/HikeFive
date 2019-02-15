d801ac1a171050c5042db6d1dea837cac9d7a9f1
unproZhai <42622194+unproZhai@users.noreply.github.com>
2019-02-14 20:49:08 -0500

    comment wall updated

--- client/src/components/post/CommentWindow.js
+++ client/src/components/post/CommentWindow.js
@@ -3,12 +3,49 @@ import PropTypes from 'prop-types';
 import CommentItem from './CommentItem';
 
 class CommentWindow extends Component {
+  constructor() {
+    super()
+    
+    this.state = {
+      itemsToShow: 3,
+      expanded: false
+    }
+
+    this.showMore = this.showMore.bind(this);
+  }
+
+  showMore() {
+
+  	const { comments, postId } = this.props;
+
+
+    this.state.itemsToShow === 3 ? (
+      this.setState({ itemsToShow: comments.length, expanded: true })
+    ) : (
+      this.setState({ itemsToShow: 3, expanded: false })
+    )
+  }
+
   render() {
     const { comments, postId } = this.props;
 
-    return comments.reverse().map(comment => (
-      <CommentItem key={comment._id} comment={comment} postId={postId} />
-    ));
+    return(
+    	<div className="container">
+                 {comments.slice(0,this.state.itemsToShow).map((comment,i) => 
+    		<li key={i}> <CommentItem key={comment._id} comment={comment} postId={postId} /> </li>
+          )}
+      
+      <p>
+        <a className="btn btn-light mr-1" onClick={this.showMore} >
+          {this.state.expanded ? (
+             <span>Show less</span>
+           ) : (
+             <span>Show more</span>
+           )
+          }
+        </a>
+      </p>
+    </div>);
   }
 }
 
@@ -16,5 +53,6 @@ CommentWindow.propTypes = {
   comments: PropTypes.array.isRequired,
   postId: PropTypes.string.isRequired
 };
+
 
 export default CommentWindow;
