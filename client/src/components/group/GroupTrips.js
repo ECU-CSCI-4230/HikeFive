4a8f4c14376e04f4a2aeebc006b09c5b28a25572
Madison <mtoquinn@gmail.com>
2019-04-02 17:16:03 -0400

    Fixed compile issue

--- client/src/components/group/GroupTrips.js
+++ client/src/components/group/GroupTrips.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Moment from 'react-moment';
-import { connect } from 'react-redux';
 import { deleteTrip } from '../../actions/groupActions';
 
 class GroupTrips extends Component {
