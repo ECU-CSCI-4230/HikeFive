c1ec1840ec82341c59e45ceb9272baa01c531ba3
Isaiah <binfordi14@students.ecu.edu>
2019-04-09 16:58:36 -0400

    uncommented

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,8 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import {
+  createStore,
+  applyMiddleware,
+  compose
+} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
@@ -11,7 +15,7 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
