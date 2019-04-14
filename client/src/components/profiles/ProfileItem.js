538884e8f8f2fa72f64bb6d02edcfca4f72d9a2b
Isaiah <binfordi14@students.ecu.edu>
2019-04-14 08:12:35 -0400

    Added default avatar images to appear in 'users' tab, but API bug still exists

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import defaultAvatar from '../../img/defaultAvatar.jpg';
+
 
 class ProfileItem extends Component {
   render() {
     const { profile } = this.props;
 
+    var profileAvatar;
+
+  if(typeof profile.avatar === 'undefined')
+  {
+    profileAvatar = defaultAvatar;
+  }
+  else
+  {
+    profileAvatar = profile.avatar;
+  }
+
     return (
       <div className="card card-body bg-light mb-3">
         <div className="row">
           <div className="col-2">
-            <img src={profile.avatar} alt="" className="rounded-circle" />
+            <img src={profileAvatar} alt="" className="rounded-circle" />
           </div>
           <div className="col-lg-6 col-md-4 col-8">
             <h3>{profile.user.name}</h3>
